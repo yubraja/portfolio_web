c4c786eaf8b79f5bc5ee51948fd74aba1cf1f54a
yubraja <yubrajadhikari987@gmail.com>
2023-07-13 08:45:02 +0545

    rotation of value

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 export const Banner=()=>{
     const [loopNum,setLoopNum]=useState(0);
-    const [isSelecting,setIsDeleting]=useState(false);
+    const [isDeleting,setIsDeleting]=useState(false);
     const toRotate=['Application Developer','Backend Developer',"UI/UX Designer"];
     const [text,setText]= useState('');
     const [delta,setDelta]= useState(300-Math.random()*100);
@@ -26,6 +26,28 @@ export const Banner=()=>{
     const tick=()=>{
 
         let i=loopNum%toRotate.length;
+        let fullText=toRotate[i];
+        let updatedText=isDeleting?fullText.substring(0,text.length-1):fullText.substring(0,text.length+1);
+
+        setText(updatedText)
+        {
+            if(isDeleting){
+                setDelta(prevDelta=>prevDelta/2);
+            }
+
+            if(!isDeleting &&updatedText==fullText){
+                setIsDeleting(true);
+                setDelta(period);
+            }
+            else if(isDeleting &&updatedText===''){
+                setIsDeleting(false);
+                setLoopNum(loopNum+1);
+                setDelta(500);
+
+            }
+
+        }
+
 
     }
     return (
@@ -34,7 +56,7 @@ export const Banner=()=>{
                 <Row className="align-items-center">
                 <Col xs={12} md={6} xl={7}>
                     <span className="tagline">Welcom to my portfolio</span>
-                    <h1>{"Hi I'm Yubraj"}<span className="wrap">App developer</span></h1>
+                    <h1>{"Hi I'm Yubraj "}<span className="wrap">{text}</span></h1>
                     <p>Who is highly enthusiastic about technology mainly on application development and Backend.
                     </p>
                     <button onClick={()=>console.log('connect')}>Let's connect<ArrowRightCircle size={25}></ArrowRightCircle></button>
