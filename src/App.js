16dc2e806531e357de4a0be314b5c2de2dd14acc
yubraja <yubrajadhikari987@gmail.com>
2023-09-21 16:47:17 +0545

    done

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <NavBar></NavBar>
-      <Banner></Banner>
+       <NavBar/>
+      <Banner />
     </div>
 
   );
